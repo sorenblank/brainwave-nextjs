44ba57dfb70409a293f9b0d4a330158c531a2d63
sorenblank <afrozaman12@gmail.com>
2024-03-26 07:26:46 +0600

    Hero structure made | Next step is adding parallax effect

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@
 import Button from "@/app/components/Button";
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
 import ButtonGradient from "@/public/svg/ButtonGradient";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <Router>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header/>
+        <Hero />
       </div>
 
       <ButtonGradient />
