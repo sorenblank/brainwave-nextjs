1c3a5f6cffec6734cbf1ab36ff5ecfe1c344bdae
sorenblank <afrozaman12@gmail.com>
2024-03-27 10:22:47 +0600

    Completed pricing section

--- app/pages/Pricing.jsx
+++ app/pages/Pricing.jsx
@@ -1,7 +1,14 @@
 // Components
 import Section from "../components/Section"
 import Img from "../components/Image"
-import Button from "../components/Button"
+import Heading from "../components/Heading"
+import PricingList from "../components/PricingList"
+
+// Ui Components
+import TagLine from "../components/Ui/Tagline"
+import { LeftLine, RightLine } from "../components/Ui/Pricing"
+
+// Assets
 import { smallSphere, stars } from "@/public"
 
 const Pricing = () => {
@@ -9,20 +16,34 @@ const Pricing = () => {
     <Section className="overflow-hidden" id="pricing">
       <div className="container relative z-2">
         <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
-          <Img 
+          <Img
             src={smallSphere}
             className="relative z-1"
             height={255}
             width={255}
             alt="Sphere" />
           <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-            <Img 
+            <Img
               src={stars}
               className="w-full"
               width={950}
               height={400}
               alt="Stars" />
           </div>
+        </div>
+
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever" />
+
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a className="text-xs font-code font-bold tracking-wider uppercase border-b" href="/pricing">See the full details</a>
         </div>
       </div>
     </Section>
