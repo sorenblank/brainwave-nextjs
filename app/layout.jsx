5f192d9e256eea0931e0795d5d039ffce377f7cd
sorenblank <afrozaman12@gmail.com>
2024-03-27 11:07:27 +0600

    Updated metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,22 @@
 import "./globals.css";
 
 export const metadata = {
-  metadataBase: "https://brainwave-ai.vercel.app", // Base URL for the website
+  metadataBase: "https://brainwave-ai-nextjs.vercel.app", // Base URL for the website
   title: "Brainwave AI", // Title of the website
   description: "Unleash the power of Ai with Brainwave. Upgrade your productivity with Brainwave, the open AI chat app.", // Description of the website
-  image: "https://brainwave-ai.vercel.app/embed.png", // Image for the website
+  image: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Image for the website
   keywords: ["Brain", "Wave", "Brainwave"], // Keywords for the website
   openGraph: { // Open Graph data for the website
     images: [
       {
-        url: "https://brainwave-ai.vercel.app/og-image.png", // Open Graph image for the website
+        url: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Open Graph image for the website
       },
     ],
   },
   twitter: { // Twitter card data for the website
     images: [
       {
-        url: "https://brainwave-ai.vercel.app/og-image.png", // Twitter card image for the website
+        url: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Twitter card image for the website
       },
     ]
   },
