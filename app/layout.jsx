575757ef598d424e8f967876dcd4042b249e5c1d
sorenblank <afrozaman12@gmail.com>
2024-04-18 15:49:37 +0600

    Extended metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,14 @@ export const metadata = {
   description: "Unleash the power of Ai with Brainwave. Upgrade your productivity with Brainwave, the open AI chat app.", // Description of the website
   image: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Image for the website
   keywords: ["Brain", "Wave", "Brainwave"], // Keywords for the website
+  type: "website",
   openGraph: { // Open Graph data for the website
+    title: "Brainwave AI", // Title for the website
+    description: "Unleash the power of Ai with Brainwave. Upgrade your productivity with Brainwave, the open AI chat app.", // Description for the website
+    image: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Open Graph image for the website
+    url: "https://brainwave-ai-nextjs.vercel.app", // URL for the website
+    type: "website",
+    siteName: "Brainwave AI", // Site name for the website
     images: [
       {
         url: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Open Graph image for the website
@@ -14,6 +21,13 @@ export const metadata = {
     ],
   },
   twitter: { // Twitter card data for the website
+    title: "Brainwave AI", // Title for the website
+    description: "Unleash the power of Ai with Brainwave. Upgrade your productivity with Brainwave, the open AI chat app.", // Description for the website
+    url: "https://brainwave-ai-nextjs.vercel.app", // URL for the website
+    domain: "brainwave-ai-nextjs.vercel.app", // Domain for the website
+    image: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Twitter card image for the website
+    cardType: "summary_large_image", // Twitter card type for the website
+    site: "@soren_blank",
     images: [
       {
         url: "https://brainwave-ai-nextjs.vercel.app/og-image.png", // Twitter card image for the website
